f0067974e55723a104390edc85a2f004cdfba2bc
Sasha <dimurchik@ukr.net>
2013-11-23 17:02:02 +0200

    add RegExp.js

---
+++ RegExp.js
@@ -0,0 +1,6 @@
+/**
+ * Created by sasha on 23.11.13.
+ */
+
+console.log( "my name is sergii".replace("sergii", "max") ); // my name is max
+console.log( "24px 56px".replace(/(\d+)px/, "#$1") ); // #24 56px
